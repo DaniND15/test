91146ca26cb1af96af7a5835905cd1ef53423f95
DaniND15 <nabicaniel174@gmail.com>
2023-10-05 23:25:36 +0100

    Je viens de changer

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -2,9 +2,6 @@ class Item:
     def __init__(self, price, weight):
         self.price = price
         self.weight = weight
-    def gain (self, p):
-        print("ls")
-        pass
 
     def pull(self, p):
-        print("ls")
+        pass
